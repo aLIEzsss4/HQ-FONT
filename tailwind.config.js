2ca2055d18328263d5847fecfab28774ebfe4acf
aLIEzsss4 <vuetomato@gmail.com>
2023-07-19 01:24:44 +0800

    feat: font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,18 @@
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './ui/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        black: '#010101',
+      }
     },
+    fontFamily: {
+      'next-book': ['var(--font-next-book)'] 
+    }
   },
   plugins: [],
 }
